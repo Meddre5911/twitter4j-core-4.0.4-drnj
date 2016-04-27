3bac730a12d0c09f5feb7d0a0484eb3c6e63b0cc
Meddre5911 <erdem.begenilmis@gmail.com>
2016-04-27 22:22:13 +0300

    Place & GeoLocation json deserialization fix

--- twitter4j-core-4.0.4/src/main/java/twitter4j/GeoLocation.java
+++ twitter4j-core-4.0.4/src/main/java/twitter4j/GeoLocation.java
@@ -23,73 +23,84 @@ package twitter4j;
  */
 public class GeoLocation implements java.io.Serializable {
 
-    private static final long serialVersionUID = 6353721071298376949L;
-    private double latitude;
-    private double longitude;
+	private static final long serialVersionUID = 6353721071298376949L;
+	private double latitude;
+	private double longitude;
 
-    /**
-     * Creates a GeoLocation instance
-     *
-     * @param latitude  the latitude
-     * @param longitude the longitude
-     */
-    public GeoLocation(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
+	/**
+	 * Creates a GeoLocation instance
+	 *
+	 * @param latitude
+	 *            the latitude
+	 * @param longitude
+	 *            the longitude
+	 */
+	public GeoLocation(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
 
-    /* For serialization purposes only. */
-    /* package */ GeoLocation() {
+	/* For serialization purposes only. */
+	public GeoLocation() {
 
-    }
+	}
 
-    /**
-     * returns the latitude of the geo location
-     *
-     * @return the latitude
-     */
-    public double getLatitude() {
-        return latitude;
-    }
+	/**
+	 * returns the latitude of the geo location
+	 *
+	 * @return the latitude
+	 */
+	public double getLatitude() {
+		return latitude;
+	}
 
-    /**
-     * returns the longitude of the geo location
-     *
-     * @return the longitude
-     */
-    public double getLongitude() {
-        return longitude;
-    }
+	/**
+	 * returns the longitude of the geo location
+	 *
+	 * @return the longitude
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof GeoLocation)) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof GeoLocation))
+			return false;
 
-        GeoLocation that = (GeoLocation) o;
+		GeoLocation that = (GeoLocation) o;
 
-        if (Double.compare(that.getLatitude(), latitude) != 0) return false;
-        if (Double.compare(that.getLongitude(), longitude) != 0) return false;
+		if (Double.compare(that.getLatitude(), latitude) != 0)
+			return false;
+		if (Double.compare(that.getLongitude(), longitude) != 0)
+			return false;
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        temp = Double.doubleToLongBits(latitude);
-        result = (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(longitude);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		temp = Double.doubleToLongBits(latitude);
+		result = (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(longitude);
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return "GeoLocation{" +
-                "latitude=" + latitude +
-                ", longitude=" + longitude +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "GeoLocation{" + "latitude=" + latitude + ", longitude=" + longitude + '}';
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
 }
